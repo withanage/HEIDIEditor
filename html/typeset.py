f8ce031e8d1f5674de1a6a7bdb262f95a5509b1a
may- <mayumi.ohta@asia-europe.uni-heidelberg.de>
2014-09-17 02:05:04 +0200

    write_json.py - save json

--- html/typeset.py
+++ html/typeset.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python
 
-from glob import glob
-# import logging
+
 import os, sys
 from xml2json import xml2json
 import optparse
 import json
 
 
-
-
-def run(upload_dir):
-    uploads = glob(upload_dir+'/*.*')
+def run(upload_dir, metadata_path):
+    uploads = os.listdir(upload_dir)
     # print uploads
 
     for u in uploads:
@@ -20,26 +17,28 @@ def run(upload_dir):
         if not filename[1] in ['doc', 'docx', 'odt']: break
 
         # stdin options
-        argv =
+        out_dir = upload_dir+"/out"
+        argv = filename[1]+" "+upload_dir+filename[0]+out_dir+" -m "+metadata_path
         meTypeset.main()
+        # give stdin options
 
-        basename = filename[0].split('/')
-        nlmfile = upload_dir+"/out/nlm/"+basename[-1]+".xml"
+        nlmfile = out_dir+"/nlm/out.xml"
         xmldata = open(nlmfile).read()
         options = optparse.Values({'pretty': False})
         jsondata = xml2json(xmldata, options, 1)
         # print jsondata
 
-        jsonfile = upload_dir+"/out/"+basename[-1]+".json"
+        jsonfile = out_dir+".json"
         with open(jsonfile, 'wb') as f:
-            json.dump(jsondata, f, sort_keys=True, indent=4)
+            json.dump("["+jsondata+"]", f, sort_keys=False, indent=4)
 
 
 if __name__ == "__main__":
     sys.path.append("/home/m1o/git/meTypeset/bin")
     import meTypeset
     UPLOAD_DIR = '/var/www/html/HEIDIEditor/html/files'
-    run(UPLOAD_DIR)
+    METADATA_PATH = '/var/www/cgi/xml/metadataTest.xml'
+    run(UPLOAD_DIR, METADATA_PATH)
 
     # jump to next page
-    # print open("step3.html", "r").read()
+    # print open("step2.html", "r").read()
